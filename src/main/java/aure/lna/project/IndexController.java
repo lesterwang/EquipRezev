e493f426e574df34af2c4379ece71238d30fc11a
Lester Wang <wanghuai28@outlook.com>
2015-07-25 14:31:49 +0800

    build the framework

---
+++ src/main/java/aure/lna/project/IndexController.java
@@ -0,0 +1,13 @@
+package aure.lna.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "html/index.html";
+    }
+}
