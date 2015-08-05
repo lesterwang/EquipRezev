bf205c1eab0f63a4621f54eaea658cd5a2c228ff
Lester Wang <wanghuai28@outlook.com>
2015-08-05 23:19:11 +0800

    add sjf4j properties config

--- src/main/java/aure/lna/project/controller/IndexController.java
+++ src/main/java/aure/lna/project/controller/IndexController.java
@@ -3,6 +3,8 @@ package aure.lna.project.controller;
 import java.util.Date;
 import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String ajax() {
+        logger.info("Hello {}", "SLF4J");
         return "index";
     }
 
